2c8cc863bc164aabbe354f85f797a411f0416c67
Sri Siva Lakshmana Reddy Dwarampudi <76223461+LakshmanGamers@users.noreply.github.com>
2022-01-25 16:06:27 +0530

    Create 6E: Design Circular Queue using array.cpp

---
+++ 6E: Design Circular Queue using array.cpp
@@ -0,0 +1,64 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    queue<int> q;
+     int n,a,b,x;
+    
+    cin >> x;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> a;
+        if(a==1){
+            cin >> b;
+            
+            if((int)q.size()<x)
+            q.push(b);
+            else
+                cout << "Queue is Full\n" ;
+        }
+        else if(a==5){
+            if(q.empty()){
+                cout << 1 << endl;
+            }
+            else{
+                cout << 0 << endl;
+            }
+            
+        }
+        else if(a==3){
+            if(!q.empty()){
+                cout << q.front() << endl;
+            }
+            else{
+                cout << "Queue is Empty\n";
+            }
+        }
+        else if(a==4){
+            if(!q.empty())
+       {
+                cout << q.back() << endl;
+            }
+            else{
+                cout << "Queue is Empty\n";
+            }
+        }
+        else if(a==2){
+            if(q.empty()){
+                cout <<"Queue is Empty\n";
+            }
+            else{
+                q.pop();
+            }
+        }
+        
+    }
+    return 0;
+}
